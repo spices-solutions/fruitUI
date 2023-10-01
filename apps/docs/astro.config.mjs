dd22c8cd6e7a9594032206a414e87cacbe5b1e99
blackPeppper <spices.digital@gmail.com>
2023-10-01 13:57:20 +0300

    CodeBlock

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,16 +4,18 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-
+import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    
+    react(),
     mdx({
       optimize: true,
       shikiConfig: { theme: "material-theme-darker" },
     }),
     partytown(),
     prefetch(),
-    sitemap(),  ],
+    sitemap(),
+    
+  ],
 });
