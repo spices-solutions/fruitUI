17a957b835b1bf1cfcf5d0965cd8e5e808157376
blackPeppper <spices.digital@gmail.com>
2023-10-30 06:46:58 +0200

    hot fix

--- packages/fruitui/src/ts/index.ts
+++ packages/fruitui/src/ts/index.ts
@@ -1,4 +1,4 @@
-import Navbar from "./ts/components/navbar";
+import Navbar from "./components/navbar";
 // import Rippel from "./ts/components/rippel";
 // import Slider from "./ts/fruitUI/slider";
 // import Alert from "./ts/components/alert";
