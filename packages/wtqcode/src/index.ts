86842c05f8534796c38bd2728f824516b05115e9
blackPeppper <spices.digital@gmail.com>
2023-11-25 20:38:52 +0200

    fix ts intellisense

--- packages/wtqcode/src/index.ts
+++ packages/wtqcode/src/index.ts
@@ -1,6 +1,7 @@
 // import Code from "./components/astro/Code.astro";
 // @ts-ignore
 export { default as CodeBlock } from "./components/astro/CodeBlock.astro";
+export type { Props as CodeBlockType } from "./components/astro/CodeBlock.astro";
 
 // export type { Props as CodeTypes } from "./components/astro/Code.astro";
 // import type { ShikiType } from "./types/shiki.ts";
