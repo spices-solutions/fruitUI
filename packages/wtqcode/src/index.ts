f1c5a7fc04b12e07a84afed2e8f87e76c6412996
blackPeppper <spices.digital@gmail.com>
2024-01-02 02:05:28 +0200

    remove stylelint to fix build

--- packages/wtqcode/src/index.ts
+++ packages/wtqcode/src/index.ts
@@ -1,8 +1,4 @@
-// import Code from "./components/astro/Code.astro";
 // @ts-ignore
 export { default as CodeBlock } from "./components/astro/CodeBlock.astro";
 // @ts-ignore
 export type { Props as CodeBlockType } from "./components/astro/CodeBlock.astro";
-
-// export type { Props as CodeTypes } from "./components/astro/Code.astro";
-// import type { ShikiType } from "./types/shiki.ts";
--- packages/wtqcode/src/types/shiki.ts
+++ packages/wtqcode/src/types/shiki.ts
@@ -1,37 +1,6 @@
-import type * as shiki from "shiki";
+export type UIComponent = HTMLElement | Element | null | undefined;
 
-export interface ShikiType {
-  /** The code to highlight. Required. */
-  code: string;
-  /**
-   * The language of your code.
-   * Supports all languages listed here: https://github.com/shikijs/shiki/blob/main/docs/languages.md#all-languages
-   * Instructions for loading a custom language: https://github.com/shikijs/shiki/blob/main/docs/languages.md#supporting-your-own-languages-with-shiki
-   *
-   * @default "plaintext"
-   */
-  lang?: shiki.Lang | shiki.ILanguageRegistration;
-  /**
-   * The styling theme.
-   * Supports all themes listed here: https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
-   * Instructions for loading a custom theme: https://github.com/shikijs/shiki/blob/main/docs/themes.md#loading-theme
-   *
-   * @default "github-dark"
-   */
-  theme?: shiki.IThemeRegistration;
-  /**
-   * Enable word wrapping.
-   *  - true: enabled.
-   *  - false: disabled.
-   *  - null: All overflow styling removed. Code will overflow the element by default.
-   *
-   * @default false
-   */
-  wrap?: boolean | null;
-  /**
-   * Generate inline code element only, without the pre element wrapper.
-   *
-   * @default false
-   */
-  inline?: boolean;
+export interface Component {
+  selector?: UIComponent,
+  
 }
