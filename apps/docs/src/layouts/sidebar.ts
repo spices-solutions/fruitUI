7bfb91d58a1930e8113cab133e37ed014771c428
blackPeppper <spices.digital@gmail.com>
2024-01-08 02:09:25 +0200

    hot fix

--- apps/docs/src/layouts/sidebar.ts
+++ apps/docs/src/layouts/sidebar.ts
@@ -3,7 +3,7 @@ const categoryItems = document.querySelectorAll(".wtq-sidebar-category li");
 categoryItems.forEach((categoryItem) => {
   const categoryItemLinks = categoryItem.querySelectorAll(".wtq-doc-link");
 
-  const collapse = categoryItem.querySelector(".wtq-sidebar-category-collapse");
+  const collapse = categoryItem.querySelector(".wtq-sidebar-category-collapse") as HTMLElement;
   const icon = categoryItem.querySelector(".wtq-sidebar-category-link");
 
   categoryItem?.addEventListener("keydown", (e) => {
@@ -36,7 +36,10 @@ categoryItems.forEach((categoryItem) => {
   });
 
   categoryItem?.addEventListener("click", () => {
-    if (categoryItem.classList.contains("wtq-active") && categoryItem.parentNode != true) {
+    if (
+      categoryItem.classList.contains("wtq-active") &&
+      categoryItem.parentNode != true
+    ) {
       categoryItemLinks.forEach((categoryItemLink) => {
         categoryItemLink.removeAttribute("tabindex");
       });
@@ -53,39 +56,12 @@ categoryItems.forEach((categoryItem) => {
     }
   });
 
-
   if (categoryItem.classList.contains("wtq-active")) {
     collapse.style.maxHeight = collapse.scrollHeight + "px";
   }
-  setTimeout(()=> {
-    collapse.style.transition = `all 
-      linear(
-        0 0%,
-        0 1.8%,
-        0.01 3.6%,
-        0.03 6.35%,
-        0.07 9.1%,
-        0.13 11.4%,
-        0.19 13.4%,
-        0.27 15%,
-        0.34 16.1%,
-        0.54 18.35%,
-        0.66 20.6%,
-        0.72 22.4%,
-        0.77 24.6%,
-        0.81 27.3%,
-        0.85 30.4%,
-        0.88 35.1%,
-        0.92 40.6%,
-        0.94 47.2%,
-        0.96 55%,
-        0.98 64%,
-        0.99 74.4%,
-        1 86.4%,
-        1 100%
-      ) 
-
-      500ms`
-  }, 500)
-  
+  setTimeout(() => {
+    if (collapse) {
+      collapse.style.transition = 'all linear( 0 0%, 0 1.8%, 0.01 3.6%, 0.03 6.35%, 0.07 9.1%, 0.13 11.4%, 0.19 13.4%, 0.27 15%, 0.34 16.1%, 0.54 18.35%, 0.66 20.6%, 0.72 22.4%, 0.77 24.6%, 0.81 27.3%, 0.85 30.4%, 0.88 35.1%, 0.92 40.6%, 0.94 47.2%, 0.96 55%, 0.98 64%, 0.99 74.4%, 1 86.4%, 1 100%) 500ms';
+    }
+  }, 500);
 });
