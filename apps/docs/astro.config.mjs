3fbce6ca3806ac6947ae4338b415b38e876254d1
blackPeppper <spices.digital@gmail.com>
2024-01-15 20:14:53 +0200

    purgecss

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -3,8 +3,9 @@ import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import AstroPWA from '@vite-pwa/astro';
-
 import compress from "astro-compress";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,49 +15,47 @@ export default defineConfig({
     shikiConfig: {
       theme: 'material-theme'
     }
-  }), partytown(), sitemap()
-
-  , compress()]
+  }), partytown(), sitemap(), compress(), purgecss()]
 });
-  // AstroPWA({
-  //   registerType: "autoUpdate",
-  //   manifest: {
-  //     name:"fruitUI",
-  //     description: "fruitUI a modular styling framework",
-  //     theme_color: "#fff",
-  //     background_color: "#18181B",
-  //     display: "standalone",
-  //     scope: "/",
-  //     start_url: "/",
-  //     orientation: "portrait",
-  //     icons: [
-  //       {
-  //         src:"/icons/treadmark-192.png",
-  //         sizes: "192x192",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src:"/icons/treadmark-512.png",
-  //         sizes: "512x512",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src:"/icons/apple-touch-icon.png",
-  //         sizes: "180x180",
-  //         type: "image/png",
-  //         purpose: "apple touch icon"
-  //       },
-  //       {
-  //         src:"/icons/teardmark-225.png",
-  //         sizes: "225x225",
-  //         type: "image/png",
-  //         purpose: "any maskable"
-  //       },
-  //       {
-  //         src:"/icons/teardmark-144.png",
-  //         sizes: "144x144",
-  //         type: "image/png",
-  //       }
-  //     ]
-  //   },
-  // })
+// AstroPWA({
+//   registerType: "autoUpdate",
+//   manifest: {
+//     name:"fruitUI",
+//     description: "fruitUI a modular styling framework",
+//     theme_color: "#fff",
+//     background_color: "#18181B",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     orientation: "portrait",
+//     icons: [
+//       {
+//         src:"/icons/treadmark-192.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//       },
+//       {
+//         src:"/icons/treadmark-512.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//       },
+//       {
+//         src:"/icons/apple-touch-icon.png",
+//         sizes: "180x180",
+//         type: "image/png",
+//         purpose: "apple touch icon"
+//       },
+//       {
+//         src:"/icons/teardmark-225.png",
+//         sizes: "225x225",
+//         type: "image/png",
+//         purpose: "any maskable"
+//       },
+//       {
+//         src:"/icons/teardmark-144.png",
+//         sizes: "144x144",
+//         type: "image/png",
+//       }
+//     ]
+//   },
+// })
