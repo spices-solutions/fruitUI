15c3c0d87f5b580bf2d78ca1e8ed87e02c57df56
blackPeppper <spices.digital@gmail.com>
2024-01-16 12:58:54 +0200

    sidebar fix, pref opt

--- apps/docs/src/layouts/sidebar.ts
+++ apps/docs/src/layouts/sidebar.ts
@@ -19,35 +19,32 @@ categoryItems.forEach((categoryItem) => {
 		".wtq-sidebar-category-link",
 	) as HTMLElement;
 
-	categoryItem?.addEventListener("click", () => {
+	icon?.addEventListener("click", () => {
 		if (categoryItem.classList.contains("wtq-active")) {
 			// biome-ignore lint/complexity/noForEach: <explanation>
 			categoryItemLinks.forEach((categoryItemLink) => {
 				categoryItemLink.setAttribute("tabindex", "-1");
 			});
+			collapse.style.maxHeight = "0px";
 			icon?.classList.remove("wtq-active");
 			categoryItem?.classList.remove("wtq-active");
-			collapse.style.maxHeight = "0";
+			collapse?.classList.remove("wtq-active");
 		} else {
-			// biome-ignore lint/complexity/noForEach: <explanation>
+      // biome-ignore lint/complexity/noForEach: <explanation>
 			categoryItemLinks.forEach((categoryItemLink) => {
-				categoryItemLink.removeAttribute("tabindex");
+        categoryItemLink.removeAttribute("tabindex");
 			});
+      collapse?.classList.remove("wtq-active");
 			collapse.style.maxHeight = `${collapse.scrollHeight}px`;
 			icon?.classList.add("wtq-active");
 			categoryItem?.classList.add("wtq-active");
 		}
 	});
 
-	setTimeout(() => {
-		if (collapse) {
-			collapse.style.transition =
-				"all linear( 0 0%, 0 1.8%, 0.01 3.6%, 0.03 6.35%, 0.07 9.1%, 0.13 11.4%, 0.19 13.4%, 0.27 15%, 0.34 16.1%, 0.54 18.35%, 0.66 20.6%, 0.72 22.4%, 0.77 24.6%, 0.81 27.3%, 0.85 30.4%, 0.88 35.1%, 0.92 40.6%, 0.94 47.2%, 0.96 55%, 0.98 64%, 0.99 74.4%, 1 86.4%, 1 100%) 500ms";
-		}
-		if (categoryItem.classList.contains("wtq-active")) {
+  
+  if (categoryItem.classList.contains("wtq-active")) {
 			collapse.style.maxHeight = `${collapse.scrollHeight}px`;
-		}
-	}, 500);
+  }
 });
 
 // biome-ignore lint/complexity/noForEach: <explanation>
