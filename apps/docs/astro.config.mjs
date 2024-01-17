6032285bcdcf30b11b36db1e669211d227571a91
blackPeppper <spices.digital@gmail.com>
2024-01-17 05:16:58 +0200

    enable pwa

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -9,53 +9,53 @@ import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://spices-solutions.github.io",
+  site: "https://fruit-ui.vercel.app/",
   integrations: [mdx({
     optimize: true,
     shikiConfig: {
       theme: 'material-theme'
     }
-  }), partytown(), sitemap(), compress(), purgecss()]
+  }), partytown(), sitemap(), compress(), purgecss(),AstroPWA({
+  registerType: "autoUpdate",
+  manifest: {
+    name:"fruitUI",
+    description: "fruitUI a modular styling framework",
+    theme_color: "#fff",
+    background_color: "#18181B",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+    icons: [
+      {
+        src:"/icons/treadmark-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src:"/icons/treadmark-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src:"/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon"
+      },
+      {
+        src:"/icons/teardmark-225.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable"
+      },
+      {
+        src:"/icons/teardmark-144.png",
+        sizes: "144x144",
+        type: "image/png",
+      }
+    ]
+  },
+})
+]
 });
-// AstroPWA({
-//   registerType: "autoUpdate",
-//   manifest: {
-//     name:"fruitUI",
-//     description: "fruitUI a modular styling framework",
-//     theme_color: "#fff",
-//     background_color: "#18181B",
-//     display: "standalone",
-//     scope: "/",
-//     start_url: "/",
-//     orientation: "portrait",
-//     icons: [
-//       {
-//         src:"/icons/treadmark-192.png",
-//         sizes: "192x192",
-//         type: "image/png",
-//       },
-//       {
-//         src:"/icons/treadmark-512.png",
-//         sizes: "512x512",
-//         type: "image/png",
-//       },
-//       {
-//         src:"/icons/apple-touch-icon.png",
-//         sizes: "180x180",
-//         type: "image/png",
-//         purpose: "apple touch icon"
-//       },
-//       {
-//         src:"/icons/teardmark-225.png",
-//         sizes: "225x225",
-//         type: "image/png",
-//         purpose: "any maskable"
-//       },
-//       {
-//         src:"/icons/teardmark-144.png",
-//         sizes: "144x144",
-//         type: "image/png",
-//       }
-//     ]
-//   },
-// })
