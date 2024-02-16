06c1502cafb0afe061d47dce82a5985e344a490b
blackPeppper <spices.digital@gmail.com>
2024-02-16 05:38:49 +0200

    migrate form files sass to scoped styles

--- packages/wtqdocs/src/blog.ts
+++ packages/wtqdocs/src/blog.ts
@@ -3,4 +3,4 @@ export { formatDate } from './utils/formatDate';
 // @ts-ignore
 export { default as BlogLayout } from "./components/BlogLayout.astro";
 // @ts-ignore
-export type { Props as BlogLayoutType } from "./components/BlogLayout.astro";
+export { default as AuthorProfile } from "./components/AuthorProfile.astro";
