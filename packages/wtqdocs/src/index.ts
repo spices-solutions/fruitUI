3aceefa4c43c19edaedb0438cddf25f422a05d8a
blackPeppper <spices.digital@gmail.com>
2024-02-29 07:47:57 +0200

    hot fix

--- packages/wtqdocs/src/index.ts
+++ packages/wtqdocs/src/index.ts
@@ -5,7 +5,7 @@ export type { Props as HeadType } from "./components/Head.astro";
 // @ts-ignore
 export { default as Navbar } from "./components/Navbar.astro";
 // @ts-ignore
-export { default as SkipLink } from "./components/Skiplink.astro";
+export { default as SkipLink } from "./components/SkipLink.astro";
 // @ts-ignore
 export { default as Layout } from "./components/Layout.astro";
 // @ts-ignore
