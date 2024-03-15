4dbf18d9851c5515da3a23d76848ea6d01472ca7
blackPeppper <spices.digital@gmail.com>
2024-03-15 14:47:55 +0200

    BrowserBlock component

--- packages/wtqcode/src/index.ts
+++ packages/wtqcode/src/index.ts
@@ -2,3 +2,5 @@
 export { default as CodeBlock } from "./components/astro/CodeBlock.astro";
 // @ts-ignore
 export type { Props as CodeBlockType } from "./components/astro/CodeBlock.astro";
+// @ts-ignore
+export { default as BrowserBlock } from "./components/astro/BrowserBlock.astro";
