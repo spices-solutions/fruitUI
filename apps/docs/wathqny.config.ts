6db32cb73c1c68e66d368b440096f94f9e217e65
blackPeppper <spices.digital@gmail.com>
2024-03-16 06:27:33 +0200

    main sidebar layout

--- apps/docs/wathqny.config.ts
+++ apps/docs/wathqny.config.ts
@@ -1,59 +1,76 @@
+interface categoryItem {
+    name: string
+    items: category
+}
+
+type category = Array<string | categoryItem>
+
 interface WathqnyConfigI {
-	siteName?: string;
-	description?: string;
-	keywords?: string[];
-  sidebar?: any;
-	image?: string;
-	category?: string[];
+  siteName?: string;
+  description?: string;
+  keywords?: string[];
+  sidebar?: category;
+  image?: string;
+  category?: any;
   darkMode?: boolean;
-	locale?: string;
+  locale?: string;
   locale_dir: "ltr" | "rtl";
-  cssPath: string[]
-	navbar?: {
-		brand: {
-			name: string;
-			logo: string;
-		};
-		Items: { href?: string; item?: string }[];
-	};
+  cssPath: string[];
+  navbar?: {
+    brand: {
+      name: string;
+      logo: string;
+    };
+    Items: { href?: string; item?: string }[];
+  };
 }
 
 const WathqnyConfig: WathqnyConfigI = {
-	siteName: "fruitUI",
-	image: "/og.png",
-	description: "fruit UI a modular styling framework",
-  keywords: ["fruitUI", "fruit", "ui", "css", "html", "js", "framework", "library","components"],
-	locale: "en",
+  siteName: "fruitUI",
+  image: "/og.png",
+  description: "fruit UI a modular styling framework",
+  keywords: [
+    "fruitUI",
+    "fruit",
+    "ui",
+    "css",
+    "html",
+    "js",
+    "framework",
+    "library",
+    "components",
+  ],
+  locale: "en",
   locale_dir: "ltr",
   darkMode: true,
-	category: ["Getting-started", "Components", "Layout", "Utilities"],
-  sidebar: {
-    gettingStarted: "Getting started",
-    components: {
-      base: 'base',
-      other: 'other',
-      awesome: 'awesome',
+  category: ["Getting-started", "Components", "Layout", "Utilities"],
+
+  sidebar: [
+    "Getting-started",
+    {
+      name: "components",
+      items: ["base", "other", "awesome"],
     },
-    layout: "Layout",
-    utilities: "Utilities"
-  },
+    "Layout",
+    "Utilities",
+  ],
   cssPath: ["./src/"],
-	navbar: {
-		brand: {
-			logo: "/icon.svg",
-			name: "fruitUI",
-		},
-		Items: [
-			{
-				item: "docs",
-				href: "/docs/getting-started/overview",
-			},
-			{
-				item: "blog",
-				href: "/blog",
-			},
-		],
-	},
+  navbar: {
+    brand: {
+      logo: "/icon.svg",
+      name: "fruitUI",
+    },
+    Items: [
+      {
+        item: "docs",
+        href: "/docs/getting-started/overview",
+      },
+      {
+        item: "blog",
+        href: "/blog",
+      },
+    ],
+  },
 };
 
 export default WathqnyConfig;
