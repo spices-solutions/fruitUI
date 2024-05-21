f4fab7774dfb131b3c2b6c8c1415bb31126b96d6
blackPeppper <spices.digital@gmail.com>
2024-05-21 12:38:01 +0300

    🐛 fix: eslint ts-ignore error

--- packages/wtqdocs/src/index.ts
+++ packages/wtqdocs/src/index.ts
@@ -1,14 +1,14 @@
-// @ts-ignore
+// @ts-expect-error Module './components/Head.astro' was resolved to ... but '--jsx' is not set.
 export { default as Head } from "./components/Head.astro";
-// @ts-ignore
+// @ts-expect-error Module './components/Head.astro' was resolved to ... but '--jsx' is not set.
 export type { Props as HeadType } from "./components/Head.astro";
-// @ts-ignore
+// @ts-expect-error Module './components/Navbar.astro' was resolved to ... but '--jsx' is not set.
 export { default as Navbar } from "./components/Navbar.astro";
-// @ts-ignore
+// @ts-expect-error Module './components/SkipLink.astro' was resolved to ... but '--jsx' is not set.
 export { default as SkipLink } from "./components/SkipLink.astro";
-// @ts-ignore
-export { default as Layout } from "./components/Layout.astro";
-// @ts-ignore
+// @ts-expect-error Module './components/SkipLink.astro' was resolved to ... but '--jsx' is not set.
+export { default as Layout } from "./components/SkipLink.astro";
+// @ts-expect-error Module './components/breadcrumbs.astro' was resolved to ... but '--jsx' is not set.
 export { default as Breadcrumbs } from "./components/breadcrumbs.astro";
-// @ts-ignore
+// @ts-expect-error Module './components/categoryCards.astro' was resolved to ... but '--jsx' is not set.
 export { default as CategoryCards } from "./components/categoryCards.astro";
