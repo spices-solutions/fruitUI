d44cf44f653c414f4cc80963fe02ca96c6e3d71d
blackPeppper <spices.digital@gmail.com>
2024-05-23 20:40:48 +0300

    🐛✨fix: build wrong import for layout component, upgrade astro 4.9

--- packages/wtqdocs/src/index.ts
+++ packages/wtqdocs/src/index.ts
@@ -7,7 +7,7 @@ export { default as Navbar } from "./components/Navbar.astro";
 // @ts-expect-error Module './components/SkipLink.astro' was resolved to ... but '--jsx' is not set.
 export { default as SkipLink } from "./components/SkipLink.astro";
 // @ts-expect-error Module './components/SkipLink.astro' was resolved to ... but '--jsx' is not set.
-export { default as Layout } from "./components/SkipLink.astro";
+export { default as Layout } from "./components/Layout.astro";
 // @ts-expect-error Module './components/breadcrumbs.astro' was resolved to ... but '--jsx' is not set.
 export { default as Breadcrumbs } from "./components/breadcrumbs.astro";
 // @ts-expect-error Module './components/categoryCards.astro' was resolved to ... but '--jsx' is not set.
