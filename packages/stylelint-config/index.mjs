7e02eb067dba1b043b7d5bb10cd0ebf560b7eb48
blackPeppper <spices.digital@gmail.com>
2024-05-28 03:43:01 +0300

    🧹 chore: loading blur effect in app browser block

--- packages/stylelint-config/index.mjs
+++ packages/stylelint-config/index.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
 
   rules: {
-    "color-function-notation": false
+    "color-function-notation": null
   },
   cache: true,
 })
--- packages/wtqcode/src/index.ts
+++ packages/wtqcode/src/index.ts
@@ -1,6 +1,3 @@
-// @ts-expect-error Module 'wtqcode/src/components/astro/Code.astro', but '--jsx' is not set.
 export { default as Code } from "./components/astro/Code.astro";
-// @ts-expect-error Module 'wtqcode/src/components/astro/Code.astro', but '--jsx' is not set.
 export type { Props as CodeType } from "./components/astro/Code.astro";
-// @ts-expect-error Module 'wtqcode/src/components/astro/BrowserBlock.astro', but '--jsx' is not set.
 export { default as BrowserBlock } from "./components/astro/BrowserBlock.astro";
