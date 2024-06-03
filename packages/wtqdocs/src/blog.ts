a4964150ef8d50005ad8e3d8da22d319b465a315
blackPeppper <spices.digital@gmail.com>
2024-06-04 00:42:40 +0300

    🐛 fix: build name conflict in blog

--- packages/wtqdocs/src/blog.ts
+++ packages/wtqdocs/src/blog.ts
@@ -1,4 +1,2 @@
-// @ts-expect-error Module './components/BlogLayout.astro' was resolved to ... but '--jsx' is not set.
-export { default as BlogLayout } from './components/layout/BlogLayout.astro'
-// @ts-expect-error Module './components/AuthorProfile.astro' was resolved to ... but '--jsx' is not set.
+export { default as BlogLayout } from './components/Layout/BlogLayout.astro'
 export { default as AuthorProfile } from './components/blog/AuthorProfile.astro'
