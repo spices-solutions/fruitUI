22e214c6f0e61e481de7440a48f6cf4dfb60fb3a
blackPeppper <spices.digital@gmail.com>
2024-06-04 02:41:13 +0300

    🎨 Lint: eslint fix task

--- packages/wtqdocs/wathqny.config.ts
+++
@@ -1,7 +0,0 @@
-import type { WathqnyConfig as WathqnyConfigType } from "wtqtypes";
-// @ts-expect-error Cannot find module '/wathqny.config' or its corresponding type declarations.
-import untypedWathqnyConfig from "/wathqny.config"
-
-let untypedWathqnyConfig: WathqnyConfigType
-
-export default untypedWathqnyConfig
