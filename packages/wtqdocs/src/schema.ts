da9fb680cc9567b19b4fc67fad5ca6df5dd0726b
blackPeppper <spices.digital@gmail.com>
2024-06-08 18:51:13 +0300

    ✨feat: biome config

--- packages/wtqdocs/src/schema.ts
+++ packages/wtqdocs/src/schema.ts
@@ -1,47 +1,53 @@
-import { slugify } from 'astro-toolkit/utils';
 import { reference, z } from 'astro:content'
+import { slugify } from 'astro-toolkit/utils'
 
-export const docSchema = ({ image }) => z
-  .object({
-    title: z.string().max(60, 'it can\'t be more than 60 characters').min(3),
-    description: z
-      .string()
-      .max(160, 'it can\'t be more than 160 characters')
-      .min(10),
-    href: z.string().optional(),
-    image: z.object({ src: z.union([image(), z.string().url()]), alt: z.string() }).optional(),
-    keywords: z.union([z.string(), z.array(z.string())]).optional(),
-    category: z.string(),
-    position: z.number().optional(),
-    authors: reference('authors').optional(),
-    draft: z.boolean().default(false),
-  })
-  .strict()
-  .transform((data) => ({
-    ...data,
-    link: `${data.href ?? `${data.category}/${slugify(data.title)}`}`,
-  }));
+export const docSchema = ({ image }) =>
+  z
+    .object({
+      title: z.string().max(60, "it can't be more than 60 characters").min(3),
+      description: z
+        .string()
+        .max(160, "it can't be more than 160 characters")
+        .min(10),
+      href: z.string().optional(),
+      image: z
+        .object({ src: z.union([image(), z.string().url()]), alt: z.string() })
+        .optional(),
+      keywords: z.union([z.string(), z.array(z.string())]).optional(),
+      category: z.string(),
+      position: z.number().optional(),
+      authors: reference('authors').optional(),
+      draft: z.boolean().default(false),
+    })
+    .strict()
+    .transform((data) => ({
+      ...data,
+      link: `${data.href ?? `${data.category}/${slugify(data.title)}`}`,
+    }))
 
-export const blogSchema = ({ image }) => z
-  .object({
-    title: z.string().max(60, 'it can\'t be more than 60 characters').min(3),
-    description: z
-      .string()
-      .max(160, 'it can\'t be more than 160 characters')
-      .min(10),
-    href: z.string().optional(),
-    image: z.object({ src: z.union([image(), z.string().url()]), alt: z.string() }).optional(),
-    keywords: z.union([z.string(), z.array(z.string())]).optional(),
-    position: z.number().optional(),
-    authors: reference('authors').optional(),
-    pubDate: z.date().transform((str: Date) => new Date(str)),
-    draft: z.boolean().default(false),
-  })
-  .strict()
-  .transform((data) => ({
-    ...data,
-    link: data.href ?? `/${slugify(data.title)}`,
-  }));
+export const blogSchema = ({ image }) =>
+  z
+    .object({
+      title: z.string().max(60, "it can't be more than 60 characters").min(3),
+      description: z
+        .string()
+        .max(160, "it can't be more than 160 characters")
+        .min(10),
+      href: z.string().optional(),
+      image: z
+        .object({ src: z.union([image(), z.string().url()]), alt: z.string() })
+        .optional(),
+      keywords: z.union([z.string(), z.array(z.string())]).optional(),
+      position: z.number().optional(),
+      authors: reference('authors').optional(),
+      pubDate: z.date().transform((str: Date) => new Date(str)),
+      draft: z.boolean().default(false),
+    })
+    .strict()
+    .transform((data) => ({
+      ...data,
+      link: data.href ?? `/${slugify(data.title)}`,
+    }))
 
 export function authorSchema({ image }: { image: any }) {
   z.object({
