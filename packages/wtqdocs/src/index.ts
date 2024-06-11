bf4d8d1631c69a80037509ac0ea7dd0f87410a27
blackPeppper <spices.digital@gmail.com>
2024-06-11 08:49:02 +0300

    ✨ feat: pagination for blog page

--- packages/wtqdocs/src/index.ts
+++ packages/wtqdocs/src/index.ts
@@ -7,7 +7,6 @@ export { default as DocsLayout } from './components/Layout/DocsLayout.astro'
 export { default as BlogLayout } from './components/Layout/BlogLayout.astro'
 export { default as Breadcrumbs } from './components/Navigation/breadcrumbs.astro'
 export { default as CategoryCards } from './components/categoryCards.astro'
-export { default as PostSummary } from './components/blog/PostSummary.astro'
 
 export { default as Code } from './components/Block/Code.astro'
 export type { Props as CodeType } from './components/Block/Code.astro'
