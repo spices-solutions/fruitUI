7f58322daf79dfc0b64d88ab503959008b521a99
blackPeppper <spices.digital@gmail.com>
2024-06-22 14:41:30 +0300

    ✨feat: mobile navbar, sidebar fixes

--- packages/wtqdocs/src/components/Navigation/navbar.ts
+++ packages/wtqdocs/src/components/Navigation/navbar.ts
@@ -10,3 +10,10 @@ document.querySelector('.theme-switcher')?.addEventListener('click', () => {
     localStorage.setItem('theme', 'dark')
   }
 })
+
+const navbarToggle = document.querySelector(".wtq-toggle")
+const navbar = document.querySelector(".wtq-links")
+
+navbarToggle?.addEventListener("click", ()=> {
+  navbar?.classList.toggle("wtq-active-links")
+})
